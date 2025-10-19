04386e2278b83e966bf35d30494e01b559f1f7a0
Abdelrhmancs <ght7234@gmail.com>
2025-10-19 15:58:44 +0300

    create product context

---
+++ src/Context/ProductContext.tsx
@@ -0,0 +1,35 @@
+import React, { createContext , useState ,useContext } from "react";
+import type { Product } from "../Comonents/Products/Product";
+
+interface ProductContextType {
+    category: string;
+    setCategory: React.Dispatch<React.SetStateAction<string>>;
+    fetch: boolean;
+    setFetch: React.Dispatch<React.SetStateAction<boolean>>;
+    details: Product;
+    setDetails: React.Dispatch<React.SetStateAction<Product>>;
+  }
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const ProductContext = createContext<ProductContextType | null>(null);
+
+export const ProductProvider = ({children} : {children: React.ReactNode}) => {
+    const [category , setCategory] = useState("");
+    const [fetch , setFetch] = useState(false);
+    const [details , setDetails] = useState<Product>({} as Product);
+    return(
+        <ProductContext.Provider value={{category , setCategory , fetch , setFetch ,details , setDetails}}>
+            {children}
+        </ProductContext.Provider>
+    )
+}
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const useProductContext = ()=>{
+    const context = useContext(ProductContext);
+    if(!context){
+        throw new Error ("useProductContext must be used inside a ProductProvider");
+    }
+    return context;
+
+}
